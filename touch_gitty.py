99ddaa1c835e37815ccf7ff1b34f0f29f4bd0891
Akos <koroknayakos@gmail.com>
2017-11-15 09:49:22 +0100

    commit 3

--- touch_gitty.py
+++ touch_gitty.py
@@ -1,15 +1,11 @@
 #dfjsaidkjaopdksaopdksaodpa
 
 def introduce():
-<<<<<<< HEAD
     print('Hello! I\'m gittie!')
 
 def add(a,b):
     c = a + b
     return c
-=======
-    print('Hello! I'm gittie!')
 
 def joke():
     print('How did Jay-Z call his girlfriend before they married?')
->>>>>>> fb4656749d5e62272f1728d1860c7a35150e89b0
